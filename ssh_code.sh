356a31d408dc28b70bfb953acec93238f7dfbcb3
vineethkumar3 <77826030+vineethkumar3@users.noreply.github.com>
2024-03-21 13:03:01 -0400

    Update ssh_code.sh

--- ssh_code.sh
+++ ssh_code.sh
@@ -6,6 +6,7 @@ gpg --export -armor ED444FF07D8D0BF6 | sudo apt-key add -
 sudo service ssh start
 echo "ssh started"
 sudo systemctl enable ssh
+sudo service ssh restart
 echo "User created"
 sudo adduser vineeth
 sudo usermod -aG sudo vineeth
