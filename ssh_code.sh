06510b42fce759b0f8ffaeb79c1b2d09834ca29c
vineethkumar3 <77826030+vineethkumar3@users.noreply.github.com>
2024-03-21 12:59:25 -0400

    Update ssh_code.sh

--- ssh_code.sh
+++ ssh_code.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "gpg key"
 gpg --recv-keys --keyserver hkp://pgp.mit.edu ED444FF07D8D0BF6
 gpg --export -armor ED444FF07D8D0BF6 | sudo apt-key add -
 sudo service ssh start
+echo "ssh started"
 sudo systemctl enable ssh
+echo "User created"
 sudo adduser vineeth
 sudo usermod -aG sudo vineeth
