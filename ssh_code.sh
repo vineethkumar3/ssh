070b24444d1f655277500182389a8075737c1fce
vineethkumar3 <77826030+vineethkumar3@users.noreply.github.com>
2024-03-23 00:31:37 -0400

    Update ssh_code.sh

--- ssh_code.sh
+++ ssh_code.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 echo "gpg key"
-gpg --recv-keys --keyserver hkp://pgp.mit.edu ED444FF07D8D0BF6
+gpg --recv-keys --keyserver hkp://keys.openpgp.org ED444FF07D8D0BF6
 gpg --export -armor ED444FF07D8D0BF6 | sudo apt-key add -
 sudo service ssh start
 echo "ssh started"
